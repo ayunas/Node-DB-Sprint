2c344d3e6bc8d43fc6ea0dfd351beaaabfd627b6
Amir Yunas <ayunas@gmail.com>
2019-09-13 12:01:53 -0500

    designed schema, and coded a migration containing all the tables, and ran the migration to initialize the projects database

---
+++ data/migrations/20190913113335_bootstrap.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('Projects', tbl => {
+      tbl.increments();
+      tbl.string('project_name',128).notNullable().unique();
+      tbl.boolean('completed').defaultTo(false);
+      tbl.string('description', 255).defaultTo('this is a new task');
+  })
+  .createTable('Resources', tbl => {
+      tbl.increments();
+      tbl.string('resource_name', 128).notNullable().unique();
+      tbl.string('resource_description', 512);
+  })
+  .createTable('Tasks', tbl => {
+      tbl.increments();
+      tbl.integer('project_id').unsigned().notNullable().references('id').inTable('Projects');
+      tbl.string('task_descripotion',512).notNullable();
+      tbl.string('task_notes', 512);
+      tbl.boolean('completed').defaultTo(false);
+  })
+  .createTable('PR_Manager', tbl => {
+      tbl.increments();
+      tbl.integer('project_id').unsigned().notNullable().references('id').inTable('Projects');
+      tbl.integer('resource_id').unsigned().notNullable().references('id').inTable('Resources');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('PR_Manager')
+  .dropTableIfExists('Tasks')
+  .dropTableIfExists('Resources')
+  .dropTableIfExists('Projects')
+};
